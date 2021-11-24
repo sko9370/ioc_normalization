db736438e7cd80b9f0d46df800c5f739dc75a486
soh <sangkeun.oh.mil@mail.mil>
2021-11-24 12:11:14 -0500

    save

--- ioc_normalization.py
+++ ioc_normalization.py
@@ -5,6 +5,7 @@ import re
 import pandas as pd
 from datetime import datetime
 from datetime import date
+from urllib.parse import urlparse
 
 # parses command line for path argument
 parser = argparse.ArgumentParser()
@@ -38,6 +39,7 @@ md5_dfs = []
 sha1_dfs = []
 sha256_dfs = []
 email_dfs = []
+ja3_dfs = []
 
 # preprocessing of input csvs
 for file_path in file_paths:
@@ -54,57 +56,80 @@ for file_path in file_paths:
             temp_df = pd.read_csv(in_file, header=0, dtype='unicode')
             # parse filename for context and date since most alienvault entries
             # do not have that information
-            filename, end = full_filename.split('_')
+            words = full_filename.split('_')
+            if len(words) == 2:
+                source, end = full_filename.split('_')
+            elif len(words) == 3:
+                attribution, source, end = full_filename.split('_')
             date_field, extension = os.path.splitext(end)
             temp_df['Published'] = date_field
             temp_df['Updated'] = date_field
             # need to replace NaN's (NONE in pandas) with empty strings to concatenate
             temp_df.fillna('', inplace=True)
-            # add filename as Context
-            temp_df.loc[temp_df['Description'] == '', 'Context'] = filename
-            temp_df.loc[temp_df['Description'] != '', 'Context'] = filename + ': ' + temp_df['Description']
+            # add source as column
+            temp_df.loc[temp_df['Description'] == '', 'Source'] = source
+            temp_df.loc[temp_df['Description'] != '', 'Source'] = source
+            # add attribution as column
+            temp_df.loc[temp_df['Description'] == '', 'Attribution'] = attribution 
+            temp_df.loc[temp_df['Description'] != '', 'Attribution'] = attribution + ': ' + temp_df['Description']
             # drop Description column now because redundant with Context
             temp_df.drop(['Description'], axis=1, inplace=True)
             # normalize column names
             temp_df.rename(columns = {'Indicator type':'Type'}, inplace=True)
             # reorder column names
-            temp_df = temp_df[['Indicator', 'Type', 'Published', 'Updated', 'Context']]
+            temp_df = temp_df[['Indicator', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
             # add dns IOCs
             dns_dfs.append(temp_df[temp_df['Type'] == 'domain'])
             dns_dfs.append(temp_df[temp_df['Type'] == 'hostname'])
             ip_dfs.append(temp_df[temp_df['Type'] == 'IPv4'])
             url_dfs.append(temp_df[temp_df['Type'] == 'URL'])
             md5_dfs.append(temp_df[temp_df['Type'] == 'FileHash-MD5'])
+            #ja3_dfs.append(temp_df[temp_df['Type'] == 'JA3'])
         # check for CrowdStrike header
         elif header == "indicator,type,malware_families,actors,reports,kill_chains,published_date,last_updated,malicious_confidence,labels":
             temp_df = pd.read_csv(in_file, header=0, dtype='unicode')
-            # drop unnecessary columns
-            unnecessary_columns = ['reports','kill_chains','malicious_confidence','malware_families','actors']
-            temp_df.drop(unnecessary_columns, axis=1, inplace=True)
+            # need to replace NaN's (NONE in pandas) with empty strings to concatenate
+            temp_df.fillna('', inplace=True)
+            # add Source and Attribution columns
+            temp_df['Source'] = 'Crowdstrike'
+            temp_df.loc[(temp_df['malware_families'] != '') & (temp_df['actors'] != ''), 'Attribution'] = 'Actors(s): ' + temp_df['actors'] + '; Malware: ' + temp_df['malware_families']
+            temp_df.loc[temp_df['malware_families'] == '', 'Attribution'] = 'Actors(s): ' + temp_df['actors']
+            temp_df.loc[temp_df['actors'] == '', 'Attribution'] = 'Malware: ' + temp_df['malware_families']
             # normalize column names
-            temp_df.rename(columns = {'indicator':'Indicator', 'type':'Type', 'published_date':'Published', 'last_updated':'Updated', 'labels': 'Context'}, inplace=True)
+            temp_df.rename(columns = {'indicator':'Indicator', 'type':'Type', 'published_date':'Published', 'last_updated':'Updated'}, inplace=True)
+            # drop unnecessary columns
+            unnecessary_columns = ['reports','kill_chains','malicious_confidence', 'labels', 'actors', 'malware_families']
+            temp_df.drop(unnecessary_columns, axis=1, inplace=True)
             # format datetime into just date YYYY-MM-DD
             temp_df['Published'] = temp_df['Published'].apply(lambda x: x[:10])
             temp_df['Updated'] = temp_df['Updated'].apply(lambda x: x[:10])
+            # reorder column names
+            temp_df = temp_df[['Indicator', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
             dns_dfs.append(temp_df[temp_df['Type'] == 'domain'])
             ip_dfs.append(temp_df[temp_df['Type'] == 'ip_address'])
             url_dfs.append(temp_df[temp_df['Type'] == 'url'])
             md5_dfs.append(temp_df[temp_df['Type'] == 'hash_md5'])
+            # ja3??
         # check for FireEye header
         # files have an odd character in line so can't do an exact match
         elif "\"Indicator Value\",\"Indicator Type\",\"Associations\",\"Exclusive\",\"First Seen\",\"Last Seen\"" in header:
             temp_df = pd.read_csv(in_file, header=0, dtype='unicode')
             temp_df.drop(['Exclusive'], axis=1, inplace=True)
             # rename columns
-            temp_df.columns = ['Indicator', 'Type', 'Context', 'Published', 'Updated']
-            # reorder columns
-            temp_df = temp_df[['Indicator', 'Type', 'Published', 'Updated', 'Context']]
+            temp_df.columns = ['Indicator', 'Type', 'Attribution', 'Published', 'Updated']
+            # add Source column
+            temp_df['Source'] = 'Mandiant FireEye'
+            # reorder column names
+            temp_df = temp_df[['Indicator', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
             temp_df['Published'] = temp_df['Published'].apply(lambda x: datetime.strptime(x, '%B %d, %Y').strftime('%Y-%m-%d'))
             temp_df['Updated'] = temp_df['Updated'].apply(lambda x: datetime.strptime(x, '%B %d, %Y').strftime('%Y-%m-%d'))
             dns_dfs.append(temp_df[temp_df['Type'] == 'FQDN'])
             ip_dfs.append(temp_df[temp_df['Type'] == 'IPV4'])
             url_dfs.append(temp_df[temp_df['Type'] == 'URL'])
             md5_dfs.append(temp_df[temp_df['Type'] == 'MD5'])
+            sha1_dfs.append(temp_df[temp_df['Type'] == 'SHA1'])
+            sha256_dfs.append(temp_df[temp_df['Type'] == 'SHA256'])
+            # ja3??
         elif ".xlsx" in full_filename:
             temp_df = pd.read_excel(file_path)
             # type 2
@@ -114,31 +139,38 @@ for file_path in file_paths:
                 temp_df['Updated'] = temp_df['date'].apply(lambda x: date.fromtimestamp(x).isoformat())
                 # need to replace NaN's (NONE in pandas) with empty strings to concatenate
                 temp_df.fillna('', inplace=True)
-                # create Context column from the filename, event_id, comment, and attribute_tag
+                # create Attribution column from the filename, event_id, comment, and attribute_tag
                 filename, ext = os.path.splitext(full_filename)
-                temp_df.loc[temp_df['comment'] != '', 'Context'] = filename + ': ' + 'Comment=' + temp_df['comment'] + '; event_id=' + temp_df['event_id'].astype(str) + '; attribute_tag=' + temp_df['attribute_tag']
-                temp_df.loc[temp_df['comment'] == '', 'Context'] = filename + ': ' + 'event_id=' + temp_df['event_id'].astype(str) + '; attribute_tag=' + temp_df['attribute_tag']
+                temp_df.loc[temp_df['comment'] != '', 'Attribution'] = filename + ': ' + 'Comment=' + temp_df['comment'] + '; event_id=' + temp_df['event_id'].astype(str) + '; attribute_tag=' + temp_df['attribute_tag']
+                temp_df.loc[temp_df['comment'] == '', 'Attribution'] = filename + ': ' + 'event_id=' + temp_df['event_id'].astype(str) + '; attribute_tag=' + temp_df['attribute_tag']
+                # create Source column
+                temp_df['Source'] = 'TF'
                 # drop unnecessary columns
                 unnecessary_columns = ['uuid', 'event_id', 'comment', 'attribute_tag', 'category', 'to_ids', 'object_relation', 'object_uuid', 'object_name', 'object_meta_category', 'date']
                 temp_df.drop(unnecessary_columns, axis=1, inplace=True)
+                # normalize column names
+                temp_df.rename(columns = {'type':'Type', 'value':'Indicator'}, inplace=True)
                 # rename columns
-                temp_df.columns = ['Type', 'Indicator', 'Published', 'Updated', 'Context']
-                # reorder columns
-                temp_df = temp_df[['Indicator', 'Type', 'Published', 'Updated', 'Context']]
+                temp_df.columns = ['Type', 'Indicator', 'Published', 'Updated', 'Attribution', 'Source']
+                # reorder column names
+                temp_df = temp_df[['Indicator', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
                 dns_dfs.append(temp_df[temp_df['Type'] == 'domain'])
                 # strip port number from ip address 
                 temp_df['Indicator'] = temp_df['Indicator'].apply(lambda x: x if '|' not in x else x.split('|')[0])
                 ip_dfs.append(temp_df[temp_df['Type'] == 'ip-dst|port'])
+                ip_dfs.append(temp_df[temp_df['Type'] == 'ip-dst'])
                 url_dfs.append(temp_df[temp_df['Type'] == 'url'])
                 md5_dfs.append(temp_df[temp_df['Type'] == 'md5'])
                 sha256_dfs.append(temp_df[temp_df['Type'] == 'sha256'])
+                email_dfs.append(temp_df[temp_df['Type'] == 'email-src'])
             # type 1
             else:
                 # need to replace NaN's (NONE in pandas) with empty strings to concatenate
                 temp_df.fillna('', inplace=True)
                 # create Context column from the filename, event_id, comment, and attribute_tag
                 filename, ext = os.path.splitext(full_filename)
-                temp_df['Context'] = filename
+                temp_df['Attribution'] = filename
+                temp_df['Source'] = 'TF'
                 # use last modified time as Published and Updated values
                 time = date.fromtimestamp(os.path.getmtime(file_path)).isoformat()
                 temp_df['Published'] = time
@@ -146,15 +178,15 @@ for file_path in file_paths:
                 # dummy column for consistency
                 temp_df['Type'] = ''
                 if 'Domains' in temp_df.columns:
-                    step_df = temp_df[['Domains', 'Type', 'Published', 'Updated', 'Context']]
+                    step_df = temp_df[['Domains', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
                     step_df.rename(columns = {'Domains':'Indicator'}, inplace=True)
                     dns_dfs.append(step_df)
                 if 'IP Addresses' in temp_df.columns:
-                    step_df = temp_df[['IP Addresses', 'Type', 'Published', 'Updated', 'Context']]
+                    step_df = temp_df[['IP Addresses', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
                     step_df.rename(columns = {'IP Addresses':'Indicator'}, inplace=True)
                     ip_dfs.append(step_df)
                 if 'URLs' in temp_df.columns:
-                    step_df = temp_df[['URLs', 'Type', 'Published', 'Updated', 'Context']]
+                    step_df = temp_df[['URLs', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
                     step_df.rename(columns = {'URLs':'Indicator'}, inplace=True)
                     url_dfs.append(step_df)
                 if 'Hashes' in temp_df.columns:
@@ -162,25 +194,25 @@ for file_path in file_paths:
                     temp_df['sha1'] = temp_df['Hashes'].apply(lambda x: x if len(x) == 40 else '')
                     temp_df['sha256'] = temp_df['Hashes'].apply(lambda x: x if len(x) == 64 else '')
 
-                    step_df = temp_df[['md5', 'Type', 'Published', 'Updated', 'Context']]
+                    step_df = temp_df[['md5', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
                     step_df = step_df[step_df['md5'].astype(bool)].copy()
                     step_df.rename(columns = {'md5':'Indicator'}, inplace=True)
                     #print(step_df)
                     md5_dfs.append(step_df)
 
-                    step_df = temp_df[['sha1', 'Type', 'Published', 'Updated', 'Context']]
+                    step_df = temp_df[['sha1', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
                     step_df = step_df[step_df['sha1'].astype(bool)].copy()
                     step_df.rename(columns = {'sha1':'Indicator'}, inplace=True)
                     #print(step_df)
                     sha1_dfs.append(step_df)
 
-                    step_df = temp_df[['sha256', 'Type', 'Published', 'Updated', 'Context']]
+                    step_df = temp_df[['sha256', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
                     step_df = step_df[step_df['sha256'].astype(bool)].copy()
                     step_df.rename(columns = {'sha256':'Indicator'}, inplace=True)
                     #print(step_df)
                     sha256_dfs.append(step_df)
                 if 'Email Addresses' in temp_df.columns:
-                    step_df = temp_df[['Email Addresses', 'Type', 'Published', 'Updated', 'Context']]
+                    step_df = temp_df[['Email Addresses', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
                     step_df.rename(columns = {'Email Addresses':'Indicator'}, inplace=True)
                     email_dfs.append(step_df)
 
@@ -190,10 +222,47 @@ if args.out_path:
 else:
     out_path = args.path
 
+# validate IP address function
+def validate_ip(address):
+    try:
+        for octet in str(address).split('.'):
+            if int(octet) < 0 or int(octet) > 255:
+                return False 
+        return True
+    except:
+        return False
+
+# parse domain function
+def get_domain(url):
+    o = urlparse(url)
+    return o.netloc.split(':')[0]
+
 # have to check if list of dfs is empty because .concat will throw error
 # deduplicate
 # write out to csv
 # check for args.wildcard commandline argument and add wildcard column if enabled
+if url_dfs:
+    url_df = pd.concat(url_dfs)
+    # parse out IPs and add to ip_dfs
+    url_df['IP'] = url_df['Indicator'].str.extract(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+    url_df.fillna('', inplace=True)
+    url_df['IP'] = url_df['IP'].apply(lambda x: x if validate_ip(x) else '')
+    temp_df = url_df[['IP', 'Type', 'Published', 'Updated', 'Attribution', 'Source']] 
+    temp_df = temp_df[temp_df['IP'].astype(bool)].copy()
+    temp_df.rename(columns = {'IP':'Indicator'}, inplace=True)
+    temp_df = temp_df[['Indicator', 'Type', 'Published', 'Updated', 'Attribution', 'Source']]
+    ip_dfs.append(temp_df)
+    # parse out domain and add to dns_dfs
+    # some domains are false positives, hold off on this
+    url_df['Domain'] = url_df['Indicator'].apply(lambda x: get_domain(x))
+    # parse out path
+    url_df['Path'] = url_df['Indicator'].str.extract(r'[\.:][a-zA-Z0-9]+(/.+$)')
+    url_df['Path'] = url_df['Path'].apply(lambda x: x if x != '\\' else '')
+    #url_df.drop(['Type', 'IP'], axis=1, inplace=True)
+    url_df.drop(['Type'], axis=1, inplace=True)
+    url_df = url_df[['Indicator', 'Domain', 'IP', 'Path', 'Published', 'Updated', 'Attribution', 'Source']]
+    url_df.drop_duplicates(subset=['Indicator'], keep='last', inplace=True)
+    url_df.to_csv(os.path.join(out_path, 'url_all.csv'), index = False)
 if dns_dfs:
     dns_df = pd.concat(dns_dfs)
     dns_df.drop(['Type'], axis=1, inplace=True)
@@ -209,12 +278,6 @@ if ip_dfs:
     #print(ip_df)
     ip_df.drop_duplicates(subset=['Indicator'], keep='last', inplace=True)
     ip_df.to_csv(os.path.join(out_path, 'ip_all.csv'), index = False)
-if url_dfs:
-    url_df = pd.concat(url_dfs)
-    url_df.drop(['Type'], axis=1, inplace=True)
-    #print(url_df)
-    url_df.drop_duplicates(subset=['Indicator'], keep='last', inplace=True)
-    url_df.to_csv(os.path.join(out_path, 'url_all.csv'), index = False)
 if md5_dfs:
     md5_df = pd.concat(md5_dfs)
     md5_df.drop(['Type'], axis=1, inplace=True)
@@ -225,12 +288,10 @@ if md5_dfs:
     # write loki format
     with open(os.path.join(out_path, 'md5_all.csv'), 'r') as in_file:
         with open(os.path.join(out_path, 'pre_loki.txt'), 'w') as file:
-            rows = csv.DictReader(in_file, fieldnames=['Indicator', 'Published', 'Updated', 'Context'])
+            rows = csv.DictReader(in_file, fieldnames=['Indicator', 'Published', 'Updated', 'Attribution', 'Source'])
             for row in rows:
-                if row['Context']:
-                    file.write(row['Indicator'] + ';' + row['Context'] + ', ' + 'Downloaded: ' + row['Published'] + '\n')
-                else:
-                    file.write(row['Indicator'] + ';' + 'Downloaded: ' + row['Published'] + '\n')
+                comment = ', '.join(filter(None, ['Attribution: ' + row['Attribution'], 'Source: ' + row['Source']]))
+                file.write(row['Indicator'] + '; ' + comment + 'Downloaded: ' + row['Published'] + '\n')
     
     # can't figure out why some extra lines starting with the previous line's description, had to manually filter them out
     with open(os.path.join(out_path, 'pre_loki.txt'), 'r') as in_file:
@@ -260,3 +321,9 @@ if email_dfs:
     #print(email_df)
     email_df.drop_duplicates(subset=['Indicator'], keep='last', inplace=True)
     email_df.to_csv(os.path.join(out_path, 'email_all.csv'), index = False)
+if len(ja3_dfs) > 0:
+    ja3_df = pd.concat(ja3_dfs)
+    ja3_df.drop(['Type'], axis=1, inplace=True)
+    #print(ja3_df)
+    ja3_df.drop_duplicates(subset=['Indicator'], keep='last', inplace=True)
+    ja3_df.to_csv(os.path.join(out_path, 'ja3_all.csv'), index = False)
