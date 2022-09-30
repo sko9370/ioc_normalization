4dc062a831a92c02a6292e604c84018933ad4cee
Sang Oh <sko9370@ymail.com>
2022-09-30 10:50:22 -0400

    progress check

--- ioc_normalization.py
+++ ioc_normalization.py
@@ -24,10 +24,9 @@ if not args.path:
 
 # expecting top level directory with sub-directories named according to source
 # alienvault (av), mandiant (md), crowdstrike (cs), threatfox (tf)
-av_files, md_files, cs_files, tf_files = utils.get_file_paths(args.path)
+av_files, md_files, cs_files, tf_files, ct_files = utils.get_file_paths(args.path)
 
-# take in different sources based on header (line 1 of csv)
-# then organize based on indicator type
+# list of dataframes to be merged at the end
 dns_dfs = []
 ip_dfs = []
 url_dfs = []
@@ -36,3 +35,37 @@ sha1_dfs = []
 sha256_dfs = []
 email_dfs = []
 ja3_dfs = []
+
+### AlienVault processing ###
+# must pass format [attribution]_[source]_[date].csv
+def parse_filename(full_filename):
+    attribution = ''
+    attribution, source, end = full_filename.split('_')
+    date_field, ext = os.path.splitext(end)
+    return attribution, source, date_field
+
+for filepath in av_files:
+    # get filename from filepath
+    filename = os.path.basename(filepath)
+    # get additional fields from filename since AlienVault CSVs only have 3 fields
+    attribution, source, date_field = parse_filename(filename)
+    # read in CSV at filepath as a dataframe
+    temp_df = pd.read_csv(filepath, header=0, dtype='unicode')
+    temp_df.fillna('', inplace=True)
+    # add attribution field based on filename
+    # uses loc to return boolean based series and specifies new column name
+    # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html
+    temp_df.loc[temp_df['Description'] == '', 'Attribution'] = attribution 
+    temp_df.loc[temp_df['Description'] != '', 'Attribution'] = attribution + ': ' + temp_df['Description']
+    # add date field based on filename
+    temp_df['Updated'] = date_field
+    # add source field based on filename
+    temp_df['Source'] = source
+    # drop Description column
+    temp_df.drop(['Description'], axis=1, inplace=True)
+    # rename columns
+    temp_df.rename(columns = {'Indicator type':'Type'}, inplace=True)
+    # reorder columns
+    # does this change in place?
+    temp_df.loc[:, ['Indicator', 'Type', 'Updated', 'Attribution', 'Source']]
+    print(temp_df)
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ def get_file_paths(topdir):
     md_files = []
     cs_files = []
     tf_files = []
+    ct_files = []
     for dirpath, dirnames, files in os.walk(topdir):
         for name in files:
             if name.lower().endswith('csv'):
@@ -14,13 +15,9 @@ def get_file_paths(topdir):
                 elif 'mandiant' in dirpath:
                     md_files.append(os.path.join(dirpath, name))
                 elif 'crowdstrike' in dirpath:
-                    md_files.append(os.path.join(dirpath, name))
+                    cs_files.append(os.path.join(dirpath, name))
                 elif 'threatfox' in dirpath:
-                    md_files.append(os.path.join(dirpath, name))
+                    tf_files.append(os.path.join(dirpath, name))
+                elif 'custom' in dirpath:
+                    ct_files.append(os.path.join(dirpath, name))
     return av_files, md_files, cs_files, tf_files 
-
-def read_csv(in_file):
-    return pd.read_csv(in_file, header=0, dtype='unicode')
-
-def fill_empty(temp_df):
-    temp_df.fillna('', inplace=True)
